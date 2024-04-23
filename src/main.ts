6007fce123ee6d5be46151538b52f48ea43eb63d
Aleksandra Plamenac <59176006+AleksandraPlamenac@users.noreply.github.com>
2024-04-23 12:15:37 +0200

    Update

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,13 @@ import { HttpClientModule } from '@angular/common/http'; // Import HttpClientMod
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
-import { CalendarUtils } from 'angular-calendar';
 
 // Modify the bootstrapApplication call to include HttpClientModule
 bootstrapApplication(AppComponent, {
   ...appConfig, // Spread existing configurations from appConfig
   providers: [
     ...(appConfig.providers || []), // Include any existing providers from appConfig
-    importProvidersFrom(HttpClientModule, CalendarUtils) // Add Providers
+    importProvidersFrom(HttpClientModule) // Add Providers
   ]
 }).catch(err => console.error(err));
 
